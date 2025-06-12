2c5da3816af969e2bd37edf16a0f8705d7e339b1
jeongsonge <jsl3126@naver.com>
2025-06-12 18:16:13 +0900

    up

---
+++ ch11/src/App NonRedux.jsx
@@ -0,0 +1,80 @@
+// import ReducerCounter1 from './ReducerCounter1'
+// import ReducerCounter2 from './ReducerCounter2'
+import { useState } from 'react'
+import './App.css'
+
+function Left1(props) {
+   return (
+      <div>
+         <h1>Left1: {props.number}</h1>
+         <Left2 number={props.number} />
+      </div>
+   )
+}
+
+function Left2(props) {
+   return (
+      <div>
+         <h1>Left2: {props.number}</h1>
+         <Left3 number={props.number} />
+      </div>
+   )
+}
+
+function Left3(props) {
+   return (
+      <div>
+         <h1>Left3: {props.number}</h1>
+      </div>
+   )
+}
+
+function Right1(props) {
+   return (
+      <div>
+         <h1>Right1</h1>
+         <Right2 onIncrease={props.onIncrease} />
+      </div>
+   )
+}
+
+function Right2(props) {
+   return (
+      <div>
+         <h1>Right2</h1>
+         <Right3 onIncrease={props.onIncrease} />
+      </div>
+   )
+}
+
+function Right3(props) {
+   return (
+      <div>
+         <h1>Right3</h1>
+         <input type="button" value="+" onClick={props.onIncrease} />
+      </div>
+   )
+}
+
+function App() {
+   //  return <ReducerCounter1 />
+   //  return <ReducerCounter2 />
+
+   const [number, setNumber] = useState(1)
+
+   return (
+      <div className="container">
+         <h1>Root: {number}</h1>
+         <div className="grid">
+            <Left1 number={number} />
+            <Right1
+               onIncrease={() => {
+                  setNumber(number + 1)
+               }}
+            />
+         </div>
+      </div>
+   )
+}
+
+export default App
